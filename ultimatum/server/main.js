9cf66084b3cb5a04f28a442e555c6c83ff393178
karnat1802 <karthiknatarajan2009@gmail.com>
2016-05-14 12:24:19 -0400

    Update main.js

--- ultimatum/server/main.js
+++ ultimatum/server/main.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
 
+
+gameinfo = new Mongo.Collection("gameInfo");
+gameList = new Mongo.Collection("Game");
